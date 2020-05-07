d984f375eee3c048efbd98addf57d681ce62cfb4
DESKTOP-E3R7G96\kenta <kentami@outlook.com>
2020-05-07 13:04:55 +0900

    修正

---
+++ mahoushoujyo/common/youtube.js
@@ -0,0 +1,39 @@
+var button = document.getElementById("mybutton");
+var youtube = document.getElementById("youtube_content");
+var ollcontent = document.getElementById("oll");
+var youtube2 = document.getElementById("youtube_content2");
+
+var myfunc = function () {
+	youtube.style.display = "none";
+	oll.style.display = "none";
+	youtube2.style.display = "none";
+}
+button.addEventListener("click", myfunc);
+
+
+var button2 = document.getElementById("yt_button");
+var myfunc2 = function () {
+	youtube.style.display = "block";
+	oll.style.display = "block";
+	youtube2.style.display = "none";
+}
+button2.addEventListener("click", myfunc2);
+
+
+
+var button3 = document.getElementById("mybutton2");
+var myfunc3 = function () {
+	youtube.style.display = "none";
+	oll.style.display = "none";
+	youtube2.style.display = "none";
+}
+button3.addEventListener("click", myfunc3);
+
+var button4 = document.getElementById("yt_button2");
+var myfunc4 = function () {
+	youtube.style.display = "none";
+	oll.style.display = "block";
+	youtube2.style.display = "block";
+}
+
+button4.addEventListener("click", myfunc4);
