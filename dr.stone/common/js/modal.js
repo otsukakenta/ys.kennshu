b788fbf3aa7dc1e352e56da585aede3738b04612
otsukakenta <k.o.k.o@i.softbank.jp>
2020-06-16 18:17:02 +0900

    no message

---
+++ dr.stone/common/js/modal.js
@@ -0,0 +1,155 @@
+var oll = document.getElementById("oll");
+
+var close_btn1 = document.getElementById("close_btn_01");
+
+var modal1 = document.getElementById("modal_content_01");
+var modal2 = document.getElementById("modal_content_02");
+var modal3 = document.getElementById("modal_content_03");
+var modal5 = document.getElementById("modal_content_05");
+var modal6 = document.getElementById("modal_content_06");
+var modal7 = document.getElementById("modal_content_07");
+var modal8 = document.getElementById("modal_content_08");
+var modal9 = document.getElementById("modal_content_09");
+var modal10 = document.getElementById("modal_content_10");
+var modal11 = document.getElementById("modal_content_11");
+var modal12 = document.getElementById("modal_content_12");
+var modal13 = document.getElementById("modal_content_13");
+var modal14 = document.getElementById("modal_content_14");
+var modal15 = document.getElementById("modal_content_15");
+var modal16 = document.getElementById("modal_content_16");
+var modal17 = document.getElementById("modal_content_17");
+var modal18 = document.getElementById("modal_content_18");
+
+var close_myfunc1 = function () {
+	oll.style.display = "none";
+	modal1.style.display = "none";
+	modal2.style.display = "none";
+	modal3.style.display = "none";
+	modal5.style.display = "none";
+	modal6.style.display = "none";
+	modal7.style.display = "none";
+	modal8.style.display = "none";
+	modal9.style.display = "none";
+	modal10.style.display = "none";
+	modal11.style.display = "none";
+	modal12.style.display = "none";
+	modal13.style.display = "none";
+	modal14.style.display = "none";
+	modal15.style.display = "none";
+	modal16.style.display = "none";
+	modal17.style.display = "none";
+	modal18.style.display = "none";
+}
+close_btn1.addEventListener("click", close_myfunc1);
+
+var open_btn1 = document.getElementById("content_01");
+var open_btn2 = document.getElementById("content_02");
+var open_btn3 = document.getElementById("content_03");
+var open_btn5 = document.getElementById("content_05");
+var open_btn6 = document.getElementById("content_06");
+var open_btn7 = document.getElementById("content_07");
+var open_btn8 = document.getElementById("content_08");
+var open_btn9 = document.getElementById("content_09");
+var open_btn10 = document.getElementById("content_10");
+var open_btn11 = document.getElementById("content_11");
+var open_btn12 = document.getElementById("content_12");
+var open_btn13 = document.getElementById("content_13");
+var open_btn14 = document.getElementById("content_14");
+var open_btn15 = document.getElementById("content_15");
+var open_btn16 = document.getElementById("content_16");
+var open_btn17 = document.getElementById("content_17");
+var open_btn18 = document.getElementById("content_18");
+
+var open_myfunc1 = function () {
+	oll.style.display = "block";
+	modal1.style.display = "block";
+}
+open_btn1.addEventListener("click", open_myfunc1);
+
+var open_myfunc2 = function () {
+	oll.style.display = "block";
+	modal2.style.display = "block";
+}
+open_btn2.addEventListener("click", open_myfunc2);
+
+var open_myfunc3 = function () {
+	oll.style.display = "block";
+	modal3.style.display = "block";
+}
+open_btn3.addEventListener("click", open_myfunc3);
+
+var open_myfunc5 = function () {
+	oll.style.display = "block";
+	modal5.style.display = "block";
+}
+open_btn5.addEventListener("click", open_myfunc5);
+
+var open_myfunc6 = function () {
+	oll.style.display = "block";
+	modal6.style.display = "block";
+}
+open_btn6.addEventListener("click", open_myfunc6);
+
+var open_myfunc7 = function () {
+	oll.style.display = "block";
+	modal7.style.display = "block";
+}
+open_btn7.addEventListener("click", open_myfunc7);
+
+var open_myfunc8 = function () {
+	oll.style.display = "block";
+	modal8.style.display = "block";
+}
+open_btn8.addEventListener("click", open_myfunc8);
+
+var open_myfunc9 = function () {
+	oll.style.display = "block";
+	modal9.style.display = "block";
+}
+open_btn9.addEventListener("click", open_myfunc9);
+
+var open_myfunc10 = function () {
+	oll.style.display = "block";
+	modal10.style.display = "block";
+}
+open_btn10.addEventListener("click", open_myfunc10);
+var open_myfunc11 = function () {
+	oll.style.display = "block";
+	modal11.style.display = "block";
+}
+open_btn11.addEventListener("click", open_myfunc11);
+var open_myfunc12 = function () {
+	oll.style.display = "block";
+	modal12.style.display = "block";
+}
+open_btn12.addEventListener("click", open_myfunc12);
+var open_myfunc13 = function () {
+	oll.style.display = "block";
+	modal13.style.display = "block";
+}
+open_btn13.addEventListener("click", open_myfunc13);
+var open_myfunc14 = function () {
+	oll.style.display = "block";
+	modal14.style.display = "block";
+}
+open_btn14.addEventListener("click", open_myfunc14);
+var open_myfunc15 = function () {
+	oll.style.display = "block";
+	modal15.style.display = "block";
+}
+open_btn15.addEventListener("click", open_myfunc15);
+var open_myfunc16 = function () {
+	oll.style.display = "block";
+	modal16.style.display = "block";
+}
+open_btn16.addEventListener("click", open_myfunc16);
+var open_myfunc17 = function () {
+	oll.style.display = "block";
+	modal17.style.display = "block";
+}
+open_btn17.addEventListener("click", open_myfunc17);
+var open_myfunc18 = function () {
+	oll.style.display = "block";
+	modal18.style.display = "block";
+}
+open_btn18.addEventListener("click", open_myfunc18);
