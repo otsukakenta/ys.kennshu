7b20ee59b3615e6face3a15ca612326e0bc0631a
otsukakenta <k.o.k.o@i.softbank.jp>
2020-09-08 18:06:59 +0900

    Dr.stoneフィードバック修正完了

--- dr.stone/common/js/tab.js
+++ dr.stone/common/js/tab.js
@@ -1,27 +1,26 @@
+var tabs = document.getElementById('tab_list').getElementsByTagName('a');
 
-        var tabs = document.getElementById('tab_list').getElementsByTagName('a');
+var pages = document.getElementById('right').getElementsByClassName('tabbox');
 
-        var pages = document.getElementById('right').getElementsByTagName('section');
+function changeTab() {
 
-        function changeTab() {
+	var targetid = this.href.substring(this.href.indexOf('#') + 1, this.href.length);
 
-            var targetid = this.href.substring(this.href.indexOf('#') + 1, this.href.length);
+	for (var i = 0; i < pages.length; i++) {
+		if (pages[i].id != targetid) {
+			pages[i].style.display = "none";
+		} else {
+			pages[i].style.display = "block";
+		}
+	}
 
-            for (var i = 0; i < pages.length; i++) {
-                if (pages[i].id != targetid) {
-                    pages[i].style.display = "none";
-                } else {
-                    pages[i].style.display = "block";
-                }
-            }
+	return false;
+}
 
-            return false;
-        }
-
-        for (var i = 0; i < tabs.length; i++) {
-            tabs[i].onclick = changeTab;
-        }
+for (var i = 0; i < tabs.length; i++) {
+	tabs[i].onclick = changeTab;
+}
 
 
-        tabs[0].onclick();
-        //tabs[0]は初めの部なのでページが読み込まれたときにここを表示するように記載。
+tabs[0].onclick();
+//tabs[0]は初めの部なのでページが読み込まれたときにここを表示するように記載。
